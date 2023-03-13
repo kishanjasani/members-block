ce81ccf5994152069725237c93fc4df625634e62
Kishan Jasani <kishanjasani007@yahoo.in>
2023-03-13 11:36:59 +0530

    Add social link styles for block editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3d6559ab546900e964f6');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '568e89e660ccf73f47cc');
