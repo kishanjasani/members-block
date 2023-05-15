5b0f9b456a64b546f281f2fb19419565c2d29e0f
Kishan <kishanjasani007@yahoo.in>
2023-05-16 00:09:58 +0530

    Fix appender style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ecee3f32928498d9ecab');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd790efb93619340c18e7');
